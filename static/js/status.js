8adcef4d9c709642825e56bda638f6c336114f6f
mycognosist <mycognosist@cryptolab.net>
2019-01-20 16:29:53 +0000

    Append IP addresses to index.html via GET request

---
+++ static/js/status.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  $.get("/ip", function(data, status){
+    console.log(data);
+    //var ap0_addr = document.getElementById("ap0_addr");
+    //var wlan0_addr = document.getElementById("wlan0_addr");
+    var ap0 = document.querySelector('#ap0_addr');
+    ap0.innerText = "ap0: " + data.ap0;
+    var wlan0 = document.querySelector('#wlan0_addr');
+    wlan0.innerText = "wlan0: " + data.wlan0;
+  });
+};
