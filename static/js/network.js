05efa9fc213baefb3ea6b9c2e2347599f0b66cfa
mycognosist <mycognosist@cryptolab.net>
2019-10-27 10:37:20 +0000

    Remove console log

--- static/js/network.js
+++ static/js/network.js
@@ -9,23 +9,21 @@ $(document).ready(function() {
     $('#wifiToggle').change(function() {
         var statusIcon = document.getElementById("statusIcon");
         if(this.checked) {
-            console.log('Activating AP Mode');
             document.getElementById("routerIcon").className = "icon-active switch-icon-right";
             document.getElementById("wifiIcon").className = "icon-inactive switch-icon-left";
             statusIcon.src = "icons/router.svg";
             $.post("/api/activate_ap",
-                function(status) {
-                    console.log("Status: " + status);
+                function() {
+                    console.log('Activating AP Mode');
                 }
             );
         } else {
-            console.log('Activating Client Mode');
             document.getElementById("wifiIcon").className = "icon-active switch-icon-left";
             document.getElementById("routerIcon").className = "icon-inactive switch-icon-right";
             statusIcon.src = "icons/wifi.svg";
             $.post("/api/activate_client",
-                function(status) {
-                    console.log("Status: " + status);
+                function() {
+                    console.log('Activating Client Mode');
                 }
             );
         }
