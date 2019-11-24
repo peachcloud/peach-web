c71d2a82fcf129958e4f6d968b765886c4874e87
mycognosist <mycognosist@cryptolab.net>
2019-11-24 15:33:39 +0000

    Add js functions for deploying ap and client

--- static/js/network.js
+++ static/js/network.js
@@ -6,26 +6,39 @@ Contains all JavaScript interactions with the peach-network microservice
 
 // AP - Client Mode Switching
 $(document).ready(function() {
-    $('#wifiToggle').change(function() {
-        var statusIcon = document.getElementById("statusIcon");
-        if(this.checked) {
-            document.getElementById("routerIcon").className = "icon-active switch-icon-right";
-            document.getElementById("wifiIcon").className = "icon-inactive switch-icon-left";
-            statusIcon.src = "icons/router.svg";
-            $.post("/api/activate_ap",
-                function() {
-                    console.log('Activating AP Mode');
-                }
-            );
-        } else {
-            document.getElementById("wifiIcon").className = "icon-active switch-icon-left";
-            document.getElementById("routerIcon").className = "icon-inactive switch-icon-right";
-            statusIcon.src = "icons/wifi.svg";
-            $.post("/api/activate_client",
-                function() {
-                    console.log('Activating Client Mode');
-                }
-            );
-        }
-    })
-})
+    $('#deployAccessPoint').click(function() {
+        console.log('Activating AP Mode');
+        fetch("/api/activate_ap", {
+            method: "post",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+        .then( (response) => {
+            console.log(response);i
+            let i = document.getElementById("netModeIcon");
+            i.src = "icons/router.svg";
+            i.className = "center icon-inactive";
+            i.value = "OFFLINE";
+        });
+    });
+
+    $('#connectWifi').click(function() {
+        console.log('Activating WiFi Client Mode');
+        fetch("/api/activate_client", {
+            method: "post",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+        .then( (response) => {
+            console.log(response);
+            let i = document.getElementById("netModeIcon");
+            i.src = "icons/wifi.svg";
+            i.className = "center icon-inactive";
+            i.value = "OFFLINE";
+        });
+    });
+});
