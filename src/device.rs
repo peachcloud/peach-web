610f466ea1ca6a45cf9e14baaef95cc206a86d06
mycognosist <mycognosist@cryptolab.net>
2020-04-07 10:58:19 +0100

    Modify shutdown and reboot commands to fire immediately

--- src/device.rs
+++ src/device.rs
@@ -1,27 +1,24 @@
 use std::io;
 use std::process::{Command, Output};
 
-/// Executes a system command to reboot the device after 5 seconds have passed.
+/// Executes a system command to reboot the device immediately.
 ///
 pub fn device_reboot() -> io::Result<Output> {
     info!("Rebooting the device");
-    // reboot after 5 seconds (allows time for JSON response)
+    // ideally, we'd like to reboot after 5 seconds to allow time for JSON
+    // response but this is not possible with the `shutdown` command alone
     Command::new("sudo")
         .arg("shutdown")
         .arg("-r")
-        .arg("-t")
-        .arg("5")
+        .arg("now")
         .output()
 }
 
-/// Executes a system command to shutdown the device after 5 seconds have passed.
+/// Executes a system command to shutdown the device immediately.
 ///
 pub fn device_shutdown() -> io::Result<Output> {
     info!("Shutting down the device");
-    // shutdown after 5 seconds (allows time for JSON response)
-    Command::new("sudo")
-        .arg("shutdown")
-        .arg("-t")
-        .arg("5")
-        .output()
+    // ideally, we'd like to reboot after 5 seconds to allow time for JSON
+    // response but this is not possible with the `shutdown` command alone
+    Command::new("sudo").arg("shutdown").arg("now").output()
 }
